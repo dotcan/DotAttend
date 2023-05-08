9e2d13858bff98a427f9c322c8774d388d573725
dotalw <15912596+dotalw@users.noreply.github.com>
2023-05-08 17:24:25 +0400

    removed block comment

--- routes/admin.php
+++ routes/admin.php
@@ -11,17 +11,6 @@ use App\Http\Controllers\RFIDScannerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::view('/', 'admin.dashboard')->name('dashboard');
 Route::resource('rfid', RFIDScannerController::class, ['except' => ['store']]);
 Route::resource('users', UserController::class);
--- routes/auth_checks.php
+++ routes/auth_checks.php
@@ -4,16 +4,5 @@ use App\Http\Controllers\AttendanceController;
 use App\Http\Controllers\ClassScheduleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::resource('attendances', AttendanceController::class, ['except' => ['create', 'edit']]);
 Route::resource('schedules', ClassScheduleController::class);
